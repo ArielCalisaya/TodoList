759184fb6f1c397ac7f7953a71230f6516256881
Ariel <calisayaariel62@gmail.com>
2019-08-03 03:06:58 -0400

    todo_list_react

---
+++ src/components/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Todo from "./appTodo.js";
+
+export class APPTODO extends React.Component {
+	render() {
+		return (
+			<div className="container-fluid">
+				<Todo />
+			</div>
+		);
+	}
+}
